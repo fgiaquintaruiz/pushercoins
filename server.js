257fd3c809ca7b65dd13328aeda2361aedea85af
fgiaquintaruiz <fgiaquintaruiz@gmail.com>
2019-11-15 07:32:10 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+// server.js
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const app = express()
+const Pusher = require('pusher')
+
+//initialize Pusher with your appId, key, secret and cluster
+const pusher = new Pusher({
+    appId: '895177',
+    key: '037fc255bcf172d1c5a9',
+    secret: 'defa63f6413c0e7e981e',
+    cluster: 'us2',
+    useTLS: true
+})
+
+// Body parser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// CORS middleware
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Set-Cookie", "SameSite=None");
+    // Pass to next layer of middleware
+    next()
+})
+
+// Set port to be used by Node.js
+app.set('port', (5000))
+
+app.get('/', (req, res) => {
+    res.send('Welcome')
+})
+
+// API route in which the price information will be sent to from the clientside
+app.post('/prices/new', (req, res) => {
+    // Trigger the 'prices' event to the 'coin-prices' channel
+    pusher.trigger( 'coin-prices', 'prices', {
+        prices: req.body.prices
+    });
+    res.sendStatus(200);
+})
+
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'))
+})
